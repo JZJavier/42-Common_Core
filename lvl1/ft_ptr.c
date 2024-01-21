8dbb5316f4b33b9ebf08dd8fb64708c7a75ab7b3
Javier Juárez Zarruk <76801285+JZJavier@users.noreply.github.com>
2024-01-21 13:55:27 +0100

    Add files via upload

---
+++ lvl1/ft_ptr.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ptr.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jjuarez- <jjuarez-@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/21 12:03:19 by jjuarez-          #+#    #+#             */
+/*   Updated: 2024/01/21 12:29:03 by jjuarez-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+static int	ft_strlen(const char *len)
+{
+	int	i;
+
+	i = 0;
+	while (len[i] != '\0')
+		i++;
+	return (i);
+}
+
+static void	rec_print(unsigned long n, const char *base, int *i)
+{
+	if (n > (unsigned long)(ft_strlen(base) - 1))
+		rec_print(n / ft_strlen(base), base, i);
+	ft_putchar(*(base + (n % ft_strlen(base))), i);
+}
+
+void	ft_ptr(void *format, int *i)
+{
+	unsigned long	n;
+	const char		*base;
+
+	n = (unsigned long)format;
+	base = "0123456789abcdef";
+	ft_putstr("0x", i);
+	rec_print(n, base, i);
+}
