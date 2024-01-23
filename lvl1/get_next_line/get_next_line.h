e52b2406cfd9d85577e19e4482c8ac1dca1450a8
Javier Juárez Zarruk <76801285+JZJavier@users.noreply.github.com>
2024-01-23 21:24:04 +0100

    Update get_next_line.h

--- lvl1/get_next_line/get_next_line.h
+++ lvl1/get_next_line/get_next_line.h
@@ -3,29 +3,30 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: javier <javier@student.42.fr>              +#+  +:+       +#+        */
+/*   By: jjuarez- <jjuarez-@tudent.42madrid.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/23 14:07:36 by javier            #+#    #+#             */
-/*   Updated: 2024/01/23 14:20:38 by javier           ###   ########.fr       */
+/*   Created: 2024/01/22 14:48:05 by jjuarez-          #+#    #+#             */
+/*   Updated: 2024/01/23 20:33:34 by jjuarez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# include <stdlib.h>
+# include <stddef.h>
 # include <unistd.h>
+# include <stdlib.h>
 
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 42
+#  define BUFFER_SIZE 10
 # endif
 
 char	*get_next_line(int fd);
-char	*ft_read_to_left_str(int fd, char *left_str);
-char	*ft_strchr(char *s, int c);
-char	*ft_strjoin(char *left_str, char *buff);
-size_t	ft_strlen(char *s);
-char	*ft_get_line(char *left_str);
-char	*ft_new_left_str(char *left_str);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_substr(char *s);
+int		ft_strchr(const char *s, int c);
+void	ft_putstr(char *s);
+size_t	ft_strlen(const char *len);
+char	*ft_strtrim(char *s1);
 
 #endif
