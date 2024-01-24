aec3738ae3244d4f64e7874abf62f73c290ac22a
Javier Juárez Zarruk <76801285+JZJavier@users.noreply.github.com>
2024-01-24 19:07:11 +0100

    Update get_next_line.h

--- lvl1/get_next_line/get_next_line.h
+++ lvl1/get_next_line/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: jjuarez- <jjuarez-@tudent.42madrid.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/22 14:48:05 by jjuarez-          #+#    #+#             */
-/*   Updated: 2024/01/23 20:33:34 by jjuarez-         ###   ########.fr       */
+/*   Updated: 2024/01/24 16:33:04 by jjuarez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,14 @@
 # include <stdlib.h>
 
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 10
+#  define BUFFER_SIZE 200
 # endif
 
 char	*get_next_line(int fd);
 char	*ft_strjoin(char *s1, char *s2);
 char	*ft_substr(char *s);
 int		ft_strchr(const char *s, int c);
-void	ft_putstr(char *s);
 size_t	ft_strlen(const char *len);
-char	*ft_strtrim(char *s1);
+char	*ft_strtrim(char const *s1);
 
 #endif
