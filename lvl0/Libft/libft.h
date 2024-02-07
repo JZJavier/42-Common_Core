13490ecf0313f7d697d04c43b52b70af783ca672
Javier Juárez Zarruk <javierjuarezarruk@gmail.com>
2024-02-07 10:11:54 +0100

    Update libft.h

--- lvl0/Libft/libft.h
+++ lvl0/Libft/libft.h
@@ -69,4 +69,7 @@ void	ft_lstclear(t_list **lst, void (*del)(void*));
 void	ft_lstiter(t_list *lst, void (*f)(void *));
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void(*del)(void *));
 
+//Funciones no solicitadas en la Libft
+int	ft_strcmp(char *s1, char *s2);
+
 #endif
