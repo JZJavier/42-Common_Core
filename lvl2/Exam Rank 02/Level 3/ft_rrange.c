e0db24795e5d9b62787c3312181a149c8ff6c07e
Javier Juárez Zarruk <javierjuarezarruk@gmail.com>
2024-02-07 09:08:11 +0100

    Add files via upload

---
+++ lvl2/Exam Rank 02/Level 3/ft_rrange.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rrange.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jjuarez- <jjuarez-@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/07 08:57:04 by jjuarez-          #+#    #+#             */
+/*   Updated: 2024/02/07 09:07:58 by jjuarez-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int *ft_rrange(int start, int end)
+{
+	int *range;
+	int i = 0;
+	int step = 1;
+	int n = end - start;
+
+	if (n < 0)
+		(n *= -1);
+	n++;
+
+	range = (int *)malloc(sizeof(int) * n);
+	if (range)
+	{
+		if (start < end)
+			step = -1;
+		while (i < n)
+		{
+			range[i] = end;
+			end = end + step;
+			i++;
+		}
+	}
+	return (range);
+}
