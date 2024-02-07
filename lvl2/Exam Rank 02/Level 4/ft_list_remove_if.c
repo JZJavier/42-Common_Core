91d50e1dd20a97f264965490c8d6e9d48796a5a4
Javier Juárez Zarruk <javierjuarezarruk@gmail.com>
2024-02-07 16:44:20 +0100

    Add files via upload

---
+++ lvl2/Exam Rank 02/Level 4/ft_list_remove_if.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list_remove_if.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jjuarez- <jjuarez-@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/07 16:38:40 by jjuarez-          #+#    #+#             */
+/*   Updated: 2024/02/07 16:44:04 by jjuarez-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+void ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)())
+{
+	if (begin_list == NULL || *begin_list == NULL)
+		return;
+
+	t_list *cur = *begin_list;
+
+	if (cmp(cur->data, data_ref) == 0)
+	{
+		*begin_list = cur->next;
+		free(cur);
+		ft_list_remove_if(begin_list, data_ref, cmp);
+	}
+}
