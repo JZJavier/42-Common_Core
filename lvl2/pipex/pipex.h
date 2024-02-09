26a997d61742d968034c3f30fb732aa5d3b53504
Javier Juárez Zarruk <javierjuarezarruk@gmail.com>
2024-02-09 06:04:47 +0100

    Add files via upload

---
+++ lvl2/pipex/pipex.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jjuarez- <jjuarez-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/07 09:33:36 by jjuarez-          #+#    #+#             */
+/*   Updated: 2024/02/09 06:01:27 by jjuarez-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include "Libft/libft.h"
+# include <unistd.h>
+# include <stdio.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>  
+# include <stdlib.h>
+
+void	parent(char **argv, int *fdd, char **envp);
+void	child(char **argv, int *fdd, char **envp);
+void	ft_exec(char *cmd, char **envp);
+char	*ft_path(char *cmd, char **envp);
+char	*ft_envp(char *variable, char **envp);
+void	ft_free(char **arr);
+int	ft_open(char *file, int in_or_out);
+
+#endif
