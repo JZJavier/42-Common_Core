7403e76064a6ada1d43ca44bebbcdea45d20ecce
Javier Juárez Zarruk <javierjuarezarruk@gmail.com>
2024-02-14 16:05:24 +0100

    LIBFT edited ft_strlenchar libft.h

--- lvl0/Libft/libft.h
+++ lvl0/Libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: jjuarez- <jjuarez-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 16:14:28 by jjuarez-          #+#    #+#             */
-/*   Updated: 2024/02/14 16:00:13 by jjuarez-         ###   ########.fr       */
+/*   Updated: 2024/02/14 16:04:50 by jjuarez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -72,7 +72,7 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void(*del)(void *));
 //Funciones no solicitadas en la Libft
 int		ft_strcmp(char *s1, char *s2);
 char	*get_next_line(int fd);
-size_t	ft_strlen(const char *len, char c);
+size_t	ft_strlenchar(const char *len, char c);
 
 
 #endif
